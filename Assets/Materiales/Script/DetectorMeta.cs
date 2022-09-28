facd31fd6ae147c67d40de44020aa7b01f82f213
Dxvah <114227774+Dxvah@users.noreply.github.com>
2022-09-28 20:45:12 +0100

    Lab ahora si que si terminadisimo

--- Assets/Materiales/Script/DetectorMeta.cs
+++ Assets/Materiales/Script/DetectorMeta.cs
@@ -1,11 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+
 
 public class DetectorMeta : MonoBehaviour
 {
    [SerializeField]
     GameObject pantallaFinal;
+
+
+    [SerializeField]
+    TextMeshProUGUI textLabTime;
+
+float tiempodePartida = 0.0f;
+bool estaJugando = true;
+
+private void Update()
+{
+    if (estaJugando == true)
+    {
+
+        tiempodePartida = tiempodePartida + Time.deltaTime;
+    }
+
+
+}
 
    private void OnTriggerEnter(Collider other)
    {
@@ -13,9 +34,11 @@ public class DetectorMeta : MonoBehaviour
     if (other.tag == "Player")
     {
 
-        Debug.Log("Jugador llegó a la meta");
+        
         pantallaFinal.SetActive(true);
         other.GetComponent<MovimientoJugador>().enabled = false;
+        estaJugando = false;
+        textLabTime.text = tiempodePartida.ToString();
     }
 
 
